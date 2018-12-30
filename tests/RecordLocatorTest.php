5ad949878ea12417036c34450c228b0100242cb1
Oliver Jakoubek <mail@oliverjakoubek.de>
2018-12-30 12:41:47 +0100

    adjusted tests to PHPUnit 7

--- tests/RecordLocatorTest.php
+++ tests/RecordLocatorTest.php
@@ -1,8 +1,8 @@
-<?php
+<?php declare(strict_types=1);
 
-require_once(__DIR__ . '/../src/RecordLocator.php');
+use PHPUnit\Framework\TestCase;
 
-class RecordLocatorTest extends PHPUnit_Framework_TestCase {
+final class RecordLocatorTest extends TestCase {
 
     private $generator;
 
